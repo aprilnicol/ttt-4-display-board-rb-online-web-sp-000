10c66ff2d6c4c90415989384e15e6d3cc4cd259e
April Adams <illusionlace@gmail.com>
2020-08-04 00:33:31 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,7 +4,7 @@ board[0] = "O"
 board[1] = "X"
 
 board = [" ", "| |" , " ", " " , "| |" , " ", " ", "| |" , " "]
-def display_board(board,row)
+def display_board(board)
   puts "#{board[0]}  | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts "#{board[3]}  | #{board[4]} | #{board[5]} "
