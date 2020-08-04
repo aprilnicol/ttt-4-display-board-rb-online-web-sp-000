7acd3a709e6f1011c36ad059c346f2bc80986c08
April Adams <illusionlace@gmail.com>
2020-08-04 00:33:26 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-row[0] = "O"
-row[1] = "x"
+board[0] = "O"
+board[1] = "X"
+
 board = [" ", "| |" , " ", " " , "| |" , " ", " ", "| |" , " "]
 def display_board(board,row)
   puts "#{board[0]}  | #{board[1]} | #{board[2]} "
